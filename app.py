605a4aec7bc1a6cfc379598da7aa6dbb4a658d90
ankit rana <ankitranastun@gmail.com>
2022-07-16 18:31:17 +0530

    ok

--- app.py
+++ app.py
@@ -158,6 +158,7 @@ def login():
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
+        #username = db.execute("SELECT username FROM users WHERE id =?", session["user_id"])[0]["username"]
 
         # Redirect user to home page
         return redirect("/")
